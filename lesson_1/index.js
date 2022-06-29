7dd24e8d10641a8945ede5ce8de77601820bc32f
huyHoangUran <huyhoangnguyen.work@gmail.com>
2022-06-29 23:36:05 +0700

    first commit

---
+++ lesson_1/index.js
@@ -0,0 +1,47 @@
+// alert("Hi every body");
+var fullName = 'Nguyễn Huy Hoàng'; //khai báo tên
+
+// alert(fullName);
+var age = 20;/**khai báo tuổi
+khai báo 
+*/
+// alert(age);
+/**
+ * name
+ * hoang
+ * 
+ */
+/**name
+ * hoang
+ * 123
+ */
+/**
+ * hàm built-in
+ * alert (in ra cảnh báo)
+ * console in ra bên console
+ *confirm
+ *prompt cho phép nhập
+ *  setTimeout đặt thời gian 1 lần
+ * setinterval đặt bao nhiêu lâu sẽ hiện 1 lần
+ */
+// console.error(fullName);
+// prompt(fullName);
+// confirm(fullName);
+// setTimeout(function() {
+//     alert(fullName);
+// },3000)
+// setInterval(function() {
+//     alert(fullName);
+// },3000);
+/**toán tử số học
+ * +    ->cộng
+ * -    ->trừ
+ * *    ->nhận
+ * /    ->chia
+ * **   ->lũy thừa
+ * %    ->chia lấy dư
+ * ++   ->tăng 1 giá trị số
+ * --   ->giảm 1 giá trị số
+ */
+var a= 15;
+console.log(a--);
